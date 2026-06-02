9244992f0a36ceada7d221ba2178b9b02e15795b
jfroy525 <jordanfreestone@gmail.com>
2026-06-02 11:22:45 -0600

    Fix vite warning and add column code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Increases the warning limit to 1.5MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Splits these heavy libraries into their own separate file to speed up initial loading
+          vendor: ['react', 'react-dom', 'recharts', 'firebase', 'lucide-react'],
+        }
+      }
+    }
+  }
 })
